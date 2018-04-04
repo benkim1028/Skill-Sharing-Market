604cba46e34085035b314b766e49ce81080740b0
Ben Kim <benkim1028@gmail.com>
2018-04-04 16:25:22 -0700

    Update reducer_alert.js

--- src/reducers/reducer_alert.js
+++ src/reducers/reducer_alert.js
@@ -1,6 +1,6 @@
 import {CREATE_ALERT_BAR, CLOSE_ALERT_BAR} from "../actions";
 
-export default function (state = {open: false, message: ""}, action) {
+export default function (state, action) {
 
     switch (action.type) {
         case CREATE_ALERT_BAR:
@@ -9,6 +9,7 @@ export default function (state = {open: false, message: ""}, action) {
         case CLOSE_ALERT_BAR:
             console.log("alert_bar getting closed");
             return {...state, open: false}
+        default:
+            return {open: false, message: ""};
     }
-    return state;
 }
