772d9277dfda14b4d1e5706f303aa996f53d5bcc
Ben Kim <benkim1028@gmail.com>
2018-04-04 16:01:08 -0700

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,13 +3,14 @@ import { reducer as formReducer } from 'redux-form';
 import authReducer from './reducer_sign_in';
 import userReducer from './reducer_profile';
 import signUpReducer from './reducer_sign_up';
-//import PostsReducer from './reducer_posts'
+import alertReducer from './reducer_alert'
 
 const rootReducer = combineReducers({
     auth: authReducer,
     form: formReducer,
     user: userReducer,
-    signup: signUpReducer
+    signup: signUpReducer,
+    alert: alertReducer
 });
 
 export default rootReducer;
