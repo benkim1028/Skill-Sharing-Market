3b96c19e8360be3c831cd55a3f532c91a9127f14
Ben Kim <benkim1028@gmail.com>
2018-04-05 12:07:50 -0700

    Update Footer.js

--- src/components/Common/Footer.js
+++ src/components/Common/Footer.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 export default class Footer extends Component{
     render() {
         return(
-            <footer className="text text-center">
+            <footer className="text text-center" style={{paddingTop: 50 + 'px'}}>
                 <p>&copy; Company 2017</p>
             </footer>
         )
