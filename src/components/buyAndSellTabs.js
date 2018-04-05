0e1337a0894c3ee1482fe688d893c5019ddc9256
Ben Kim <benkim1028@gmail.com>
2018-04-05 13:46:23 -0700

    Update buyAndSellTabs.js

--- src/components/buyAndSellTabs.js
+++ src/components/buyAndSellTabs.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import {Tabs, Tab} from 'material-ui/Tabs';
+import Header from "./Common/Header";
+import Footer from "./Common/Footer";
+
 
 const styles = {
   headline: {
@@ -27,27 +30,31 @@ export default class BuyAndSellTabs extends React.Component {
 
   render() {
     return (
-      <Tabs
-        value={this.state.value}
-        onChange={this.handleChange}
-      >
-        <Tab label="Buy" value="buy">
-          <div>
-            <h2 style={styles.headline}>Buy Tab</h2>
-            <p>
-              This tab will be used for Buying
-            </p>
-          </div>
-        </Tab>
-        <Tab label="Sell" value="sell">
-          <div>
-            <h2 style={styles.headline}>Sell Tab</h2>
-            <p>
-              This tab will be used for selling.
-            </p>
-          </div>
-        </Tab>
-      </Tabs>
+      <div>
+        <Header />
+        <Tabs
+          value={this.state.value}
+          onChange={this.handleChange}
+        >
+          <Tab label="Buy" value="buy">
+            <div>
+              <h2 style={styles.headline}>Buy Tab</h2>
+              <p>
+                This tab will be used for Buying
+              </p>
+            </div>
+          </Tab>
+          <Tab label="Sell" value="sell">
+            <div>
+              <h2 style={styles.headline}>Sell Tab</h2>
+              <p>
+                This tab will be used for selling.
+              </p>
+            </div>
+          </Tab>
+        </Tabs>
+        <Footer />
+      </div>
     );
   }
 }
