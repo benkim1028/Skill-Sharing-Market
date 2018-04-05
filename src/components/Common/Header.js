14d287fa0433e5a4e96191562fe149dc5cf395da
Ben Kim <benkim1028@gmail.com>
2018-04-05 12:00:09 -0700

    Update Header.js

--- src/components/Common/Header.js
+++ src/components/Common/Header.js
@@ -1,30 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import AppBar from 'material-ui/AppBar';
 import LoginButton from "../../containers/login_button"
 import DrawerMenu from "../drawer";
 import history from "../../history";
 
-/**
- * A simple example of `AppBar` with an icon on the right.
- * By default, the left icon is a navigation-menu.
- */
-const selectStyle = () => {
-    if(this.props.main)
-        return {position: 'absolute', backgroundColor: 'transparent'}
-    else 
-        return {}
+class Header extends Component{
+    constuctor(props){
+        super(props);
+    }
+    
+    selectStyle(){
+        if(this.props.main)
+            return {position: 'absolute', backgroundColor: 'transparent'}
+        else 
+            return {}
+    }
+
+
+    render(){ 
+        return(
+            <AppBar
+                style={this.selectStyle()}
+                title="WITHUMB"
+                zDepth={0}
+                onTitleClick={() => history.push('/')}
+                iconElementRight={<LoginButton/>}
+                iconElementLeft={<DrawerMenu />}
+            />
+        );
+    }
 }
-
-
-const Header = () => (
-    <AppBar
-        style={selectStyle()}
-        title="WITHUMB"
-        zDepth={0}
-        onTitleClick={() => history.push('/')}
-        iconElementRight={<LoginButton/>}
-        iconElementLeft={<DrawerMenu />}
-    />
-);
 
 export default Header;
