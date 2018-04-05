4f3928605b346be35abad28461f576a501211d9d
Ben Kim <benkim1028@gmail.com>
2018-04-05 13:52:56 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,21 +5,28 @@ import reduxThunk from 'redux-thunk';
 import {Provider} from "react-redux";
 import {createStore, applyMiddleware} from 'redux';
 import {Router, Switch, Route} from 'react-router-dom';
+import history from './history'
+
 import {SIGN_IN_SUCCESSFUL} from './actions'
+
 import 'bootstrap/dist/css/bootstrap.css';
 import './css/index.css';
 
 import reducers from './reducers';
-import SignIn from './containers/sign_in'
-import SignUp from './containers/sign_up'
+
 import Main from './components/Main'
 import Secret from './components/Secret'
+import Udemy from "./components/Udemy";
+import BuyAndSellTabs from "./components/buyAndSellTabs"
+
+import SignIn from './containers/sign_in'
+import SignUp from './containers/sign_up'
 import requireAuth from './containers/required_auth';
 import noRequireAuth from './containers/no_require_auth';
 import AlertBar from './containers/alert_bar';
 import Loading from './containers/loading';
-import history from './history'
-import Udemy from "./components/Udemy";
+
+
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
@@ -42,6 +49,7 @@ ReactDOM.render(
             <Router history={history}>
                 <div>
                     <Switch>
+                        <Route path="/buyNsell" component={BuyAndSellTabs}>
                         <Route path="/udemy" component={Udemy}/>
                         <Route path="/secret" component={requireAuth(Secret)}/>
                         <Route path="/profile" component={requireAuth(Profile)}/>
